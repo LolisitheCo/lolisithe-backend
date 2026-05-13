91063a507d3764b36eb31c096d73b8445d561dab
LolisitheCo <sivuyilematras@gmail.com>
2026-05-13 18:53:06 +0200

    fix render firebase env

--- server.js
+++ server.js
@@ -17,13 +17,11 @@ if (!admin.apps.length) {
       clientEmail:
         process.env.FIREBASE_CLIENT_EMAIL,
 
-      privateKey:
-  process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(
-        /\\n/g,
-        "\n"
-      )
-    : undefined,
+    privateKey:
+  process.env.FIREBASE_PRIVATE_KEY.replace(
+    /\\n/g,
+    "\n"
+  ),
     }),
   });
 }
