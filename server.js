0e1dcc37e967e5dc01dd399f981606394fa6ef03
LolisitheCo <sivuyilematras@gmail.com>
2026-05-19 19:01:40 +0200

    Fix production CORS

--- server.js
+++ server.js
@@ -68,7 +68,6 @@ app.use(
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
 
       "https://lolisitheco.co.za",
 
@@ -101,7 +100,6 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
 
       "https://lolisitheco.co.za",
 
