4d1ddf996f25d34438c7be2a24069f7e55d2f5b4
LolisitheCo <sivuyilematras@gmail.com>
2026-05-20 16:27:42 +0200

    Fix admin auth and stats route

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -2,12 +2,18 @@ const axios = require("axios");
 const admin = require("firebase-admin");
 const crypto = require("crypto");
 
+const admin = require("firebase-admin");
+
 if (!admin.apps.length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY
+    .replace(/\\n/g, "\n")
+    .replace(/^"|"$/g, "");
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: privateKey,
     }),
   });
 }
